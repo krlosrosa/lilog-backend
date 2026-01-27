b245f08760d48feb68a6869b7b40a841c7673f3c
carlos <robertorsilva.krlos@gmail.com>
2026-01-27 09:24:46 -0300

    adjustes de pasta

--- src/infra/guards/keycloak.config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('keycloak', () => ({
-  url: process.env.KEYCLOAK_URL,
-  realm: process.env.REALM_NAME,
-  clientId: 'backend',
-  clientSecret: process.env.CLIENT_SECRET_KEYCLOAK,
-}));
