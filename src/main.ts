e5551020274164da5c7f6b19aa3e8c506f0c42b4
carlos <robertorsilva.krlos@gmail.com>
2026-01-27 09:56:47 -0300

    Add Swagger documentation setup and update dependencies
    - Added @nestjs/swagger version 11.2.5 to package.json and package-lock.json
    - Created swagger.config.ts to configure Swagger documentation
    - Updated main.ts to initialize Swagger and set global API prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './main/app.module';
+import { setupSwagger } from './main/docs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app); // 2. Chame a função para configurar o Swagger
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
